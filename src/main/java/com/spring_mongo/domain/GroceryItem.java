7faa98b6da7ec42efcbe72c80dad37ecd8261414
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-25 01:37:43 -0600

    add tag Document groceryitems from org.springframework.data.mongodb.core.mapping.Document

--- src/main/java/com/spring_mongo/domain/GroceryItem.java
+++ src/main/java/com/spring_mongo/domain/GroceryItem.java
@@ -3,10 +3,12 @@ package com.spring_mongo.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document("groceryitems")
 public class GroceryItem {
 
   private String id;
