49bd35e0241ed93ca34c860ce8b627da4292a7f8
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-25 01:37:10 -0600

    add tags from lombock into domain GroceryItem

--- src/main/java/com/spring_mongo/domain/GroceryItem.java
+++ src/main/java/com/spring_mongo/domain/GroceryItem.java
@@ -1,5 +1,17 @@
 package com.spring_mongo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GroceryItem {
+
+  private String id;
+  private String name;
+  private int quantity;
+  private String category;
 
 }
