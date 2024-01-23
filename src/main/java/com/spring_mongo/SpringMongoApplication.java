008b5cf3c7b2a5e95d81a3b99c2d897a9d416402
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-23 00:20:34 -0600

    spring initialized the Application Context using the SpringBootApplication annotation

--- src/main/java/com/spring_mongo/SpringMongoApplication.java
+++ src/main/java/com/spring_mongo/SpringMongoApplication.java
@@ -2,8 +2,10 @@ package com.spring_mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class SpringMongoApplication {
 
 	public static void main(String[] args) {
