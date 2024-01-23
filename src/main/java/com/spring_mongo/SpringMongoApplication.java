20fc4604e3460b8dbba00e3ff641b4a3f068d0c9
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-23 00:33:16 -0600

    implement method createGroceryItems() just for create instances of GroceryItems

--- src/main/java/com/spring_mongo/SpringMongoApplication.java
+++ src/main/java/com/spring_mongo/SpringMongoApplication.java
@@ -1,5 +1,8 @@
 package com.spring_mongo;
 
+import com.spring_mongo.domain.GroceryItem;
+import com.spring_mongo.repository.GroceryItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,12 +12,27 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories
 public class SpringMongoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMongoApplication.class, args);
-	}
+  @Autowired
+  GroceryItemRepository groceryItemRepository;
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMongoApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    createGroceryItems();
+  }
+
+  void createGroceryItems() {
+    System.out.println("Data creation started... ");
+
+    groceryItemRepository.save(new GroceryItem("Whole Wheat Biscuit", "Whole Wheat Biscuit", 5, "snacks")))
+    groceryItemRepository.save(new GroceryItem("Kodo Millet", "XYZ Kodo Millet healthy", 2, "millets"));
+    groceryItemRepository.save(new GroceryItem("Dried Red Chilli", "Dried Whole Red Chilli", 2, "spices"));
+    groceryItemRepository.save(new GroceryItem("Pearl Millet", "Healthy Pearl Millet", 1, "millets"));
+    groceryItemRepository.save(new GroceryItem("Cheese Crackers", "Bonny Cheese Crackers Plain", 6, "snacks"));
+
+  }
+
 }
