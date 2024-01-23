3086395aa4e66774ed01412856fb16448851b8e2
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-23 00:26:42 -0600

    add Id and change the name of collection

--- src/main/java/com/spring_mongo/domain/GroceryItem.java
+++ src/main/java/com/spring_mongo/domain/GroceryItem.java
@@ -1,10 +1,12 @@
 package com.spring_mongo.domain;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("groceryitems")
+@Document("GroceryItem")
 public class GroceryItem {
 
+  @Id
   private String id;
   private String name;
   private int quantity;
