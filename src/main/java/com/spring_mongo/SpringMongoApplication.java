757d1a26680200bea19d01a2c42c589289b7995e
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-23 00:28:19 -0600

    implement CommandLineRunner for create object

--- src/main/java/com/spring_mongo/SpringMongoApplication.java
+++ src/main/java/com/spring_mongo/SpringMongoApplication.java
@@ -1,15 +1,20 @@
 package com.spring_mongo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class SpringMongoApplication {
+public class SpringMongoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMongoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
 }
