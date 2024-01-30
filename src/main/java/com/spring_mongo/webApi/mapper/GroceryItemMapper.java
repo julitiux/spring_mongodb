2373725d7e1964e107437006a21ae72603c70e41
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-29 23:24:11 -0600

    implement od static method and return a new GroceryItemDto

--- src/main/java/com/spring_mongo/webApi/mapper/GroceryItemMapper.java
+++ src/main/java/com/spring_mongo/webApi/mapper/GroceryItemMapper.java
@@ -2,11 +2,16 @@ package com.spring_mongo.webApi.mapper;
 
 import com.spring_mongo.domain.GroceryItem;
 import com.spring_mongo.webApi.domain.GroceryItemCommand;
+import com.spring_mongo.webApi.domain.GroceryItemDto;
 
 public class GroceryItemMapper {
 
   public static GroceryItem of(final GroceryItemCommand command){
     return new GroceryItem(null, command.name(), command.quantity(), command.category());
+  }
+
+  public static GroceryItemDto of(final GroceryItem groceryItem){
+    return new GroceryItemDto(groceryItem.getName(), groceryItem.getQuantity(), groceryItem.getCategory());
   }
 
 }
