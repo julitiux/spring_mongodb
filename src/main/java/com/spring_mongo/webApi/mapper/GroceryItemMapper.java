f66f2e8ba7c310a631a0670758aaa75ef10175c3
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-29 23:11:18 -0600

    implement static method of GroceryItem

--- src/main/java/com/spring_mongo/webApi/mapper/GroceryItemMapper.java
+++ src/main/java/com/spring_mongo/webApi/mapper/GroceryItemMapper.java
@@ -1,5 +1,12 @@
 package com.spring_mongo.webApi.mapper;
 
+import com.spring_mongo.domain.GroceryItem;
+import com.spring_mongo.webApi.domain.GroceryItemCommand;
+
 public class GroceryItemMapper {
+
+  public static GroceryItem of(final GroceryItemCommand command){
+    return new GroceryItem(null, command.name(), command.quantity(), command.category());
+  }
 
 }
