6b537ffa02a6924ca2aa4def0764186e4acf4845
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-31 16:32:59 -0600

    implement of method static; recive List<GroceryItemCommand> to List<GroceryItem>

--- src/main/java/com/spring_mongo/webApi/mapper/GroceryItemMapper.java
+++ src/main/java/com/spring_mongo/webApi/mapper/GroceryItemMapper.java
@@ -4,10 +4,17 @@ import com.spring_mongo.domain.GroceryItem;
 import com.spring_mongo.webApi.domain.GroceryItemCommand;
 import com.spring_mongo.webApi.domain.GroceryItemDto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GroceryItemMapper {
 
   public static GroceryItem of(final GroceryItemCommand command){
     return new GroceryItem(null, command.name(), command.quantity(), command.category());
+  }
+
+  public static List<GroceryItem> of(List<GroceryItemCommand> itemCommands){
+    return itemCommands.stream().map(GroceryItemMapper::of).collect(Collectors.toList());
   }
 
   public static GroceryItemDto of(final GroceryItem groceryItem){
